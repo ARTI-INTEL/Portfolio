aa3eef77a154918e1c04542b95af03b3d0bd953e
ARTI-INTEL <faiqimran.fi@gmail.com>
2024-08-08 17:37:05 +0400

    Visuals Completed

--- script.js
+++ script.js
@@ -2,3 +2,11 @@
 // I'm looking forward to learning more about software, machine learning, artificial intelligence, cyber security, and how technology works in our daily lives.
 // I already have experience from doing many projects at home during and after my studies, but I believe this course will help me develop my talents and apply them effectively.
 // My other passions include photography, videography, and photo and video editing, which I publish on Instagram. I've been playing cricket since I was in fourth grade, and I volunteer.
+
+let MenuIcon = document.querySelector("#menu-icon")
+let navbar = document.querySelector(".navbar")
+
+MenuIcon.onclick = () => {
+    MenuIcon.classList.toggle("bx-x")
+    navbar.classList.toggle('active')
+}
